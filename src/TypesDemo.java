b25570a31cb2ed2918fb84ea212fd0dd30aa01f9
manoj <manojs121@rediffmail.com>
2020-05-20 12:20:12 +0530

    20-05-2020

---
+++ src/TypesDemo.java
@@ -0,0 +1,21 @@
+
+public class TypesDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Long.SIZE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        /*
+        System.out.println(Short.SIZE/8);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        */
+        /*
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        */
+        
+    }
+}

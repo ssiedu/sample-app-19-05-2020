9ee7b32dd601e2e21129c9d58166a6ac98131500
manoj <manojs121@rediffmail.com>
2020-05-21 12:23:12 +0530

    21-05-2020

---
+++ src/ConversionExample.java
@@ -0,0 +1,15 @@
+
+public class ConversionExample {
+
+    public static void main(String[] args) {
+
+        float v1=(float)2.5;   
+        
+    }
+}
+
+/*
+    1) put f and make it a float value.
+    2) take your variable type double.
+    3) type-conversion (cast-operator)
+*/
--- src/Trial.java
+++ src/Trial.java
@@ -1,5 +1,15 @@
+
 public class Trial {
-    void show(){
-        System.out.println("hello from show of Trial");
+
+    static boolean b1;
+    static Boolean b2;
+    
+    //static double d1;
+    //static Double d2;
+    
+    public static void main(String[] args) {
+        System.out.println(b1);
+        System.out.println(b2);
     }
+
 }

4a6bf3a0b25dce380f16fc99bea0660b593088f3
manoj <manojs121@rediffmail.com>
2020-05-27 12:17:17 +0530

    27-05-2020

--- src/OperatorExample.java
+++ src/OperatorExample.java
@@ -1,8 +1,36 @@
+class Checking{
+    
+}
+
 
 public class OperatorExample {
 
     public static void main(String[] args) {
-
-        System.out.println('A'=='A');
+        
+        int n=10;
+        //n+=5;   //n=n+5
+        //n-=4;       //n=n-4
+        //n*=2;       //n=n*2
+        //n/=2;           //n=n/2;
+        n%=4;           //n=n%3;
+        System.out.println(n);
+        
+        
+        
+        //System.out.println(!true);
+        //System.out.println(!false);
+        
+        /*
+        System.out.println(true||true);
+        System.out.println(true||false);
+        System.out.println(false||true);
+        System.out.println(false||false);
+        */
+        /*
+        System.out.println(true&&true);
+        System.out.println(true&&false);
+        System.out.println(false&&true);
+        System.out.println(false&&false);
+        */
     }
 }
